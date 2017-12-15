a2e75024b02022d191833050e7ee23c925691f77
Rubén Antón <rubocoptero@gmail.com>
2017-12-15 13:23:42 +0100

    Red acceptance test

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -5,7 +5,13 @@ class BankAccount
 end
 
 describe BankAccount do
-  it '' do
+  it 'prints a bank statement' do
+    bank_account = BankAccount.new
 
+    bank_account.deposit(1000)
+    bank_account.deposit(2000)
+    bank_account.withdrawal(500)
+
+    expect(bank_account.balance).to eq(2500)
   end
 end
