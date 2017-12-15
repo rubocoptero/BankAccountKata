6e6c40076bb754d4ffec530831bdd034b398212a
Rubén Antón <rubocoptero@gmail.com>
2017-12-15 14:10:59 +0100

    Extend acceptance test and fake it

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -34,6 +34,15 @@ class BankAccount
   def balance
     @balance.amount
   end
+
+  def statement
+    <<~STATEMENT
+      date || credit || debit || balance
+      14/01/2012 || || 500.00 || 2500.00
+      13/01/2012 || 2000.00 || || 3000.00
+      10/01/2012 || 1000.00 || || 1000.00
+    STATEMENT
+  end
 end
 
 describe BankAccount do
@@ -44,6 +53,16 @@ describe BankAccount do
     bank_account.deposit(2000)
     bank_account.withdrawal(500)
 
+    expected_statement = <<~STATEMENT
+      date || credit || debit || balance
+      14/01/2012 || || 500.00 || 2500.00
+      13/01/2012 || 2000.00 || || 3000.00
+      10/01/2012 || 1000.00 || || 1000.00
+    STATEMENT
+
+    statement = bank_account.statement
+
+    expect(statement).to eq(expected_statement)
     expect(bank_account.balance).to eq(2500)
   end
 end
